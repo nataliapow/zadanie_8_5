b54e30c23971afae1d58477429689ff7e022a24b
Natalia <n.powalska@gmail.com>
2017-08-06 20:07:21 +0200

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var a = prompt('Enter the base a of a triangle');
+var h = prompt('Enter the height h of a triangle');
+triangleArea = 4 * h / 2;
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
